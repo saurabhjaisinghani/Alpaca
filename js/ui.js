fd24b1f7c8b950ff1b0b601751139ddd75c7603c
saurabhjaisinghani <49756649+saurabhjaisinghani@users.noreply.github.com>
2021-11-15 23:27:58 +0530

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -2,6 +2,24 @@ class UI {
   constructor() {
     this.mainCategory = '';
     this.subCategory = '';
-    
+  }
+  
+  showMainCategories = mainCategories => {
+    mainCategories.forEach((mainCategories, index) => {
+      const mainCategoryWrap = document.querySelector(
+        '.main-button-collection'
+        );
+        mainCategoryWrap.appendChild(
+          this.createButtonWrap('main', mainCategory, index)
+        );
+    });
+  };
+  
+  showSubButtonHeading(heading) {
+    document.getElementById('sub-button-heading').innerText=heading;
+  }
+  
+  showSubCategories = (mainCategory, index) => {
+    const subCategories = 
   }
 }
